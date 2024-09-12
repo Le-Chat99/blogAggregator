ef7dd01cd1b13da79d30c2fedaa10bef5b29b32f
Le-Chat99 <bgkhiem291@gmail.com>
2024-09-12 16:00:57 +0700

    fixed, 5/11

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/Le-Chat99/blogAggregator
 go 1.23.1
 
 require (
+	github.com/google/uuid v1.6.0 // indirect
 	github.com/joho/godotenv v1.5.1 // indirect
 	github.com/lib/pq v1.10.9 // indirect
 )
--- main.go
+++ main.go
@@ -1,14 +1,17 @@
 package main
 
 import (
+	"context"
 	"database/sql"
 	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
+	"time"
 
 	"github.com/Le-Chat99/blogAggregator/internal/database"
+	"github.com/google/uuid"
 	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
 )
@@ -34,7 +37,7 @@ func main() {
 
 	mux.HandleFunc("GET /v1/healthz", healthz)
 	mux.HandleFunc("GET /v1/err", errHfunc)
-	mux.HandleFunc("POST /v1/users")
+	mux.HandleFunc("POST /v1/users", cfg.userAdd)
 
 	srv := &http.Server{
 		Addr:    ":" + port,
@@ -62,10 +65,22 @@ func (cfg *apiConfig) userAdd(w http.ResponseWriter, r *http.Request) {
 	}
 	decoder := json.NewDecoder(r.Body)
 	params := Params{}
-	err = decoder.Decode(&params)
+	err := decoder.Decode(&params)
 	if err != nil {
 		msg := fmt.Sprintf("Error decoding parameters: %s", err)
 		respondWithError(w, http.StatusInternalServerError, msg)
 		return
 	}
+	user := database.CreateUserParams{
+		ID:        uuid.New(),
+		UpdatedAt: time.Now(),
+		Name:      params.Name,
+	}
+	createdUser, err := cfg.DB.CreateUser(context.Background(), user)
+	if err != nil {
+		msg := fmt.Sprintf("Error create user fail: %s", err)
+		respondWithError(w, http.StatusInternalServerError, msg)
+		return
+	}
+	respondWithJSON(w, http.StatusCreated, createdUser)
 }
